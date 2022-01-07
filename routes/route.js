67cd81704956d5b341d9ab0f3d2a66b13c5c096a
zakaria2105 <zelar903@gmail.com>
2022-01-07 16:47:32 +0100

    create router

---
+++ routes/route.js
@@ -0,0 +1,19 @@
+const departmentController = require("./../controllers/departmentController");
+const usersController = require("./../controllers/usersController");
+
+module.exports = function (express) {
+  const route = express.Router(); //brands route
+  route.get("/department", departmentController.getAll);
+  route.get("/department/:id", departmentController.getOne);
+  route.post("/department", departmentController.create);
+  route.put("/department/:id", departmentController.update);
+  route.delete("/department/:id", departmentController.delete);
+  route.get("/departmentWithUsers", departmentController.getDepartmentUsers); //users route
+  route.get("/user", userController.getAll);
+  route.get("/user/:id", userController.get);
+  route.post("/user", userController.create);
+  route.put("/user/:id", userController.update);
+  route.delete("/user/:id", userController.delete);
+  return route;
+};
+module.exports = self;
