317df8ccdc8159fa0b02e69dc4d99554311f0ec6
zakaria2105 <zelar903@gmail.com>
2022-01-07 11:16:38 +0100

    change tables name to lowercase

--- migrations/20220106144053-create-department.js
+++ migrations/20220106144053-create-department.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("Departments", {
+    await queryInterface.createTable("departments", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -27,6 +27,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable("Departments");
+    await queryInterface.dropTable("departments");
   },
 };
