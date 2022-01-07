3012b6539ac7e72ae7652b6f22549a0f6abb0b70
zakaria2105 <zelar903@gmail.com>
2022-01-07 16:48:10 +0100

    fix route

--- routes/route.js
+++ routes/route.js
@@ -2,13 +2,13 @@ const departmentController = require("./../controllers/departmentController");
 const usersController = require("./../controllers/usersController");
 
 module.exports = function (express) {
-  const route = express.Router(); //brands route
+  const route = express.Router(); 
   route.get("/department", departmentController.getAll);
   route.get("/department/:id", departmentController.getOne);
   route.post("/department", departmentController.create);
   route.put("/department/:id", departmentController.update);
   route.delete("/department/:id", departmentController.delete);
-  route.get("/departmentWithUsers", departmentController.getDepartmentUsers); //users route
+  route.get("/departmentWithUsers", departmentController.getDepartmentUsers);
   route.get("/user", userController.getAll);
   route.get("/user/:id", userController.get);
   route.post("/user", userController.create);
