173009ade0c8579102df386e64e1a2560b37f55a
zakaria2105 <zelar903@gmail.com>
2022-01-10 10:36:11 +0100

    create get one department function

--- controllers/departmentController.js
+++ controllers/departmentController.js
@@ -5,15 +5,27 @@ const { sequelize, Department } = require("../models");
 
 async function getAll(req, res) {
   try {
-    const department = await Department.findAll();
-    return res.json(department);
+    const departments = await Department.findAll();
+    return res.json(departments);
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error.message });
   }
 }
 
-app.get("/", getAll);
+async function getOne(req, res) {
+    const id = req.params.id;
+    try {   
+      const department = await Department.findOne({ where: { id } });
+      return res.json(department);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: error.message });
+    }
+}
+
+app.get("/departments", getAll);
+app.get("/department/:id", getOne);
 
 app.listen({ port: 8000 }, async () => {
   console.log("server running on http://localhost:8000");
