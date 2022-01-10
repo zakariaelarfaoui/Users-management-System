6e5780d21fbb9e2c8d799907cf6669e6a441db76
zakaria2105 <zelar903@gmail.com>
2022-01-10 11:20:23 +0100

    POST methode for department

--- controllers/departmentController.js
+++ controllers/departmentController.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 const { sequelize, Department } = require("../models");
 
@@ -14,23 +15,32 @@ async function getAll(req, res) {
 }
 
 async function getOne(req, res) {
-    const id = req.params.id;
-    try {   
-      const department = await Department.findOne({ where: { id } });
-      return res.json(department);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: error.message });
-    }
+  const id = req.params.id;
+  try {
+    const department = await Department.findOne({ where: { id } });
+    return res.json(department);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+}
+
+
+async function create(req, res) {
+  const { name, description } = req.body;
+  try {
+    const department = await Department.create({ name, description });
+    return res.json(department);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
 }
 
 app.get("/departments", getAll);
 app.get("/department/:id", getOne);
+app.post("/departments", create);
 
-app.listen({ port: 8000 }, async () => {
+app.listen({ port: 8000 }, () => {
   console.log("server running on http://localhost:8000");
-  await sequelize.authenticate();
-  if (sequelize.authenticate()) {
-    console.log("connection established");
-  }
 });
