6c0079668dc7eb552a7456c9ae79481c185052d7
zakaria2105 <zelar903@gmail.com>
2022-01-11 14:23:52 +0100

    user crud

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,9 +1,9 @@
-const { sequelize, User, Department } = require("../models");
+const { sequelize, Department, User } = require("../models");
 
 const usersController = {};
 
 usersController.create = async (req, res) => {
-  const { id, name, email, password, departmentId } = req.body;
+  const { id, name, email, password } = req.body;
   try {
     const department = await Department.findOne({ where: id });
     const user = await User.create({
@@ -14,6 +14,60 @@ usersController.create = async (req, res) => {
     });
     return res.json(user);
     return res.json({ message: "row inserted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+usersController.getAll = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    return res.json(users);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+usersController.getOne = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await User.findOne({ where: { id } });
+    return res.json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+usersController.delete = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await User.findOne({ where: { id } });
+    await user.destroy();
+    return res.json({ message: "row deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+usersController.update = async (req, res) => {
+  const id = req.params.id;
+  const { name, email, password, departmentId } = req.body;
+  try {
+    const user = await User.findOne({ where: { id } });
+
+    user.name = name;
+    user.email = email;
+    user.password = password;
+    user.departmentId = departmentId;;
+
+    await user.save();
+
+    return res.json({ message: "row updated successfully" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error.message });
