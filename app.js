7cfcf18bc9de7f10cfe7dfc58f13501bd127e0c3
zakaria2105 <zelar903@gmail.com>
2022-01-11 15:56:53 +0100

    use express router

--- app.js
+++ app.js
@@ -2,21 +2,10 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const { departmentController } = require("./controllers/departmentController");
-const { usersController } = require("./controllers/usersController");
+// const {departmentRoutes} = require("./routes/departments")
+const { userRoutes } = require("./routes/users");
 
-app.get("/departments", departmentController.getAll);
-app.get("/department/:id", departmentController.getOne);
-app.post("/departments", departmentController.create);
-app.delete("/department/:id", departmentController.delete);
-app.put("/department/:id", departmentController.update);
-
-app.get("/users", usersController.getAll);
-app.get("/user/:id", usersController.getOne);
-app.post("/users", usersController.create);
-app.delete("/user/:id", usersController.delete);
-app.put("/user/:id", usersController.update);
-app.get("/departmentUsers/:id", usersController.departmentUsers);
+app.use("/", userRoutes);
 
 app.listen({ port: 8000 }, () => {
   console.log("server running on http://localhost:8000");
