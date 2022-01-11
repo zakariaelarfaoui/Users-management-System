e9e7e7f9bb3abdb6482635556f8b31a87add1613
zakaria2105 <zelar903@gmail.com>
2022-01-11 14:06:31 +0100

    fix foreignkey name

--- models/department.js
+++ models/department.js
@@ -7,9 +7,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({Users}) {
+    static associate({User}) {
       // define association here
-      this.hasMany(Users, {foreignKey: "id"})
+      this.hasMany(User, { foreignKey: "departmentId" });
     }
   }
   Department.init(
