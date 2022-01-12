b85097536a0a472c26feedd84c2b41c7c8c6cb4e
zakaria2105 <zelar903@gmail.com>
2022-01-12 16:08:22 +0100

    use body parser

--- app.js
+++ app.js
@@ -2,21 +2,25 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
 
-app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js")); // redirect bootstrap JS
-app.use("/js", express.static(__dirname + "/node_modules/jquery/dist")); // redirect JS jQuery
-app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css")); // redirect CSS bootstrap
+app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js")); 
+app.use("/js", express.static(__dirname + "/node_modules/jquery/dist"));
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 
 app.use(
   "/stylesheets/fontawesome",
   express.static(__dirname + "/node_modules/@fortawesome/fontawesome-free/")
 );
+
 const departmentRoutes = require("./routes/departments");
 const userRoutes = require("./routes/users");
-
 app.use("/", userRoutes, departmentRoutes);
 
 app.listen({ port: 8000 }, () => {
