f2dc7b80a8749596bbffae16341d50840a96b38c
zakaria2105 <zelar903@gmail.com>
2022-01-13 13:31:38 +0100

    use morgame and change static folder placr

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const express = require("express");
-const app = express();
-app.use(express.json());
+const morgan = require("morgan");
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const departmentRoutes = require("./routes/departments");
+const userRoutes = require("./routes/users");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/", userRoutes, departmentRoutes);
+app.use(morgan('dev'))
 
 app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + "/public"));
-
+app.use(express.static(__dirname + "/"));
 app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js")); 
 app.use("/js", express.static(__dirname + "/node_modules/jquery/dist"));
 app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
-
 app.use(
   "/stylesheets/fontawesome",
   express.static(__dirname + "/node_modules/@fortawesome/fontawesome-free/")
 );
 
-const departmentRoutes = require("./routes/departments");
-const userRoutes = require("./routes/users");
-app.use("/", userRoutes, departmentRoutes);
 
 app.listen({ port: 8000 }, () => {
   console.log("server running on http://localhost:8000");
