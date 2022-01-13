0e936b37431d636ff9b698cb6f28a6f0cf622af6
zakaria2105 <zelar903@gmail.com>
2022-01-13 16:50:00 +0100

    nothing

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const userRoutes = require("./routes/users");
 
 const app = express();
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", userRoutes, departmentRoutes);
 app.use(morgan('dev'))
