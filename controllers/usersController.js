5b66a70ecbf6d5a1a0490b9c38c00c780d617cdd
zakaria2105 <zelar903@gmail.com>
2022-01-14 15:57:56 +0100

    edit update function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -26,7 +26,7 @@ usersController.getOne = async (req, res) => {
   const id = req.params.id;
   User.findOne({ where: { id } })
     .then((result) => {
-        res.render("users/user", { user: result });
+      res.render("users/details", { user: result });
     })
     .catch((error) => {
       console.log(error);
@@ -47,22 +47,20 @@ usersController.delete = async (req, res) => {
 
 usersController.update = async (req, res) => {
   const id = req.params.id;
-  const { name, email, password, departmentId } = req.body;
-  try {
-    const user = await User.findOne({ where: { id } });
-
-    user.name = name;
-    user.email = email;
-    user.password = password;
-    user.departmentId = departmentId;
-
-    await user.save();
-
-    return res.json({ message: "row updated successfully" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error.message });
-  }
+  // res.send(req.body)
+  const { newUserName, newUserEmail, newUserDepartment } = req.body;
+  User.findOne({ where: { id } })
+    .then((result) => {
+      result.name = newUserName;
+      result.email = newUserEmail;
+      result.departmentID = newUserDepartment;
+      result.save();
+      res.redirect("/users");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ err: err.message });
+    });
 };
 
 usersController.departmentUsers = async (req, res) => {
